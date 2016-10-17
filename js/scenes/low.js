5d2ce2ed2cb22a20083cc86a8a63e12784e79f6e
Numaer Zaker <znzaker@gmail.com>
2016-10-17 16:47:05 -0400

    condense lowmag scene. get rid of text tooltip redundancy by merging into core step functions. previously, it seemed counterintuitve to double declare functions instead of merging them into one

--- js/scenes/low.js
+++ js/scenes/low.js
@@ -5,151 +5,89 @@
  * Third scene that teaches student how to use the microscope 
  */
 
-// ===== Hardcoded Bounds ===== //
+// ===== Utility Functions ===== //
 
-var bounds = {}
-
-
-// ====== Start Trigger ======= //
-
-function textSetupEyepiece() {
-    var arr = ["#eyepiece", "#ocularRight", "#ocularLeft", "#ocularLensBase"];
-    popupOn("Adjust the eyepiece to change the magnification.", {
-            "left": "5%",
-            "top": "25%",
-            });
+function textSetup(tooltip, lt, tp){
+    popupOn(tooltip, {"left": lt, "top": tp});
 }
 
-function textSetupDiaphragmLight() {
-    arr = ["#diaphragm", "#apertureFixed", "#aperture"];
-    popupOn("Adjust the diaphragm to change the lighting on the slide.", {
-            "left": "10%",
-            "top": "60%",
-            });
-}
-
-
-function textSetupDiaphragmHeight() {
-    arr = ["#diaphragm", "#apertureFixed", "#aperture"];
-    popupOn("Adjust the height of the diaphragm by rotating the knob.", {
-            "left": "50%",
-            "top": "60%",
-            });
-}
-
-function textSetupFine() {
-    arr = ["#knobsFine"];
-    popupOn("Move the stage up a little to sharpen the focus", {
-            "left": "10%",
-            "top": "64%",
-            });
-}
-
-function textSetupCoarse() {
-    arr = ["#knobsCoarse"];
-    popupOn("Move the stage up by moving the coarse knob.", {
-            "left": "10%",
-            "top": "64%",
-            });
-}
-
-function textSetupCaliper() {
-    arr = ["#caliper", "#xcaliper", "#ycaliper", "#caliperKnob"];
-    popupOn("Move the caliper to adjust the position of the slide.", {
-            "left": "62%",
-            "top": "60%",
-            });
-}
-
-function textSetupLenses() {
-    arr = ["#lenses", "#lensesRed", "#lensesBlue", "#lensesYellow", "#lensesWhite"];
-    edgeArr = arr;
-    popupOn("Lenses: The lenses are rotated on the nosepiece to change the magnification. These different lenses are referred to as the objectives.", {
-            "left": "10%",
-            "top": "36%",
-            });
-}
-
-
-/* Property to inspect, lower bound, upper bound, engine piece, div piece, remove listener*/
-function subHandler(prop, low, upper, piece, components, handler){
+// Property to inspect, lower bound, upper bound, engine piece, div piece, remove listener
+function subHandler(prop, low, upper, piece, doc, handler){
     if(prop > low && prop < upper){ 
         piece.complete();
-        $(components).unbind("mousemove", handler);               
+        $(doc).unbind("mousemove", handler);               
     }
 }
 
+// ====== Start Trigger Listeners ======= //
+
+
 function lowAdjustEyepiece() {
-    textSetupEyepiece();
-    if (lowEyepiece.isActive()) {
-        var components = "#ocularRight, #ocularLeft" 
+    textSetup("Adjust the eyepiece to change the magnification.", "5%", "25%");
+    if (lowEyepiece.isActive()) { 
         var handler = function(){
-            subHandler(microscope.eyepiecePosition, 10, 25, lowEyepiece, components, handler);
+            subHandler(microscope.eyepiecePosition, 10, 25, lowEyepiece, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
 
 /*Trigger for coarse knob (for now I have conjoined them)*/
 function lowAdjustCoarse() {
-    textSetupCoarse();
+    textSetup("Move the stage up by moving the course knob.", "10%", "64%");
     if (lowCoarse.isActive()) { 
-        var components = "#knobsCoarse" 
         var handler = function(){
-            subHandler(microscope.knobPosition, -30, 0, lowCoarse, components, handler);
+            subHandler(microscope.knobPosition, -30, 0, lowCoarse, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
 
 /*Trigger for fine knob (for now I have conjoined them)*/
 
 function lowAdjustFine() {
-    textSetupFine();
-    if (lowFine.isActive()) {
-        var components = "#knobsFine" 
+    textSetup("Move the stage up slightly by moving the fine knob.", "10%", "64%");
+    if (lowFine.isActive()) { 
         var handler = function(){
-            subHandler(microscope.knobPosition, -30, -10, lowFine, components, handler);
+            subHandler(microscope.knobPosition, -30, -10, lowFine, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
 
 
 // Adjust diaphram light
 function lowDLight() {
-    textSetupDiaphragmLight();
-    if (lowDiaphragmLight.isActive()) {
-        var components = "#diaphragm" 
+    textSetup("Adjust the diaphragm to change the lighting on the slide.", "10%", "60%");
+    if (lowDiaphragmLight.isActive()) { 
         var handler = function(){
-            subHandler(microscope.diaphragmLightPosition, 5, 30, lowDiaphragmLight, components, handler);
+            subHandler(microscope.diaphragmLightPosition, 5, 30, lowDiaphragmLight, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
 
 
 function lowDHeight() {
-    textSetupDiaphragmHeight();
+    textSetup("Adjust the height of the diaphragm by rotating the knob.", "50%", "60%");
     if (lowDiaphragmHeight.isActive()) {
-        var components = document 
         var handler = function(){
-            subHandler(microscope.diaphragmHeightPosition, 5, 15, lowDiaphragmHeight, components, handler);
+            subHandler(microscope.diaphragmHeightPosition, 5, 15, lowDiaphragmHeight, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
 
 
 function lowAdjustCaliper() {
-    textSetupCaliper();
+    textSetup("Move the caliper to adjust the position of the slide.", "62%", "60%");
     if (lowCaliper.isActive()) {
-        var components = document 
+
         var handler = function(){
-            subHandler(microscope.xcaliper, 5, 20, lowCaliper, components, handler);
-            subHandler(microscope.ycaliper, 5, 20, lowCaliper, components, handler);
+            subHandler(microscope.xcaliper, 5, 20, lowCaliper, document, handler);
+            subHandler(microscope.ycaliper, 5, 20, lowCaliper, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
 
         }
     
@@ -157,12 +95,11 @@ function lowAdjustCaliper() {
 
 
 function lowAdjustLenses() {
-    textSetupLenses();
+    textSetup("Lenses: The lenses are rotated on the nosepiece to change the magnification. These different lenses are referred to as the objectives.", "10%", "36%"); 
     if (lowLenses.isActive()) {
-        var components = document 
         var handler = function(){
-            subHandler(microscope.diaphragmHeightPosition, 5, 15, lowLenses, components, handler);
+            subHandler(microscope.diaphragmHeightPosition, 5, 15, lowLenses, document, handler);
         }
-        $(components).bind("mousemove", handler);
+        $(document).bind("mousemove", handler);
     }
 }
