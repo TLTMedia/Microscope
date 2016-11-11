4f91256212bd64603d23f0677572f18c9e370b8f
Numaer Zaker <znzaker@gmail.com>
2016-11-11 17:47:30 -0500

    apply shift on bounds to keep bounds consistent despite delta y on platform

--- js/scene-manager.js
+++ js/scene-manager.js
@@ -22,7 +22,7 @@ function bindTooltip(){
    Use of below function is for intro.js (intro.js + gameLogic.js interdependence).
  */
 function startStep(step) {
-    var isDebug = false;
+    var isDebug = true;
 
     /* intro
 
