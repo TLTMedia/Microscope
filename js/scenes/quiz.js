b8c5805c5c86e3beffa5b904e2630aaee43c97a9
Numaer Zaker <znzaker@gmail.com>
2017-03-30 18:58:39 -0400

    post-write to server working

--- js/scenes/quiz.js
+++ js/scenes/quiz.js
@@ -6,11 +6,15 @@
 
 function quizMag1(){
     answer = "100X"
-    $("#answerBox button").click(function(){
-        inputAnswer = $("#answerBox input").val();
-        if (inputAnswer == answer){
-            totalMagQ1.complete(); 
-        }
-        $("#answerBox input").val("");
-    });
+        $("#answerBox button").click(function(){
+            inputAnswer = $("#answerBox input").val();
+            $.post( "./api/quizzes", { "answer": inputAnswer })
+            .done(function( inputAnswer ) {
+                if (inputAnswer == answer){
+                    totalMagQ1.complete(); 
+                }
+                $("#answerBox input").val("");
+            });
+
+        });
 }
