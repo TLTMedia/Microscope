a830cf12dad949df45a3ce07dec25e262496bfb3
Wesley Nam <wesleynam1@gmail.com>
2017-04-10 18:58:10 -0400

    Made changes to the magnification quiz.

--- js/scenes/quiz.js
+++ js/scenes/quiz.js
@@ -3,7 +3,7 @@
  *
  * Quiz on microscope usage.
  */
-
+/*
 function quizMag1(){
     answer = "400X";
     ms.rotateLensesCount(ms, true, true, function(){}, 1); 
@@ -20,6 +20,7 @@ function quizMag1(){
         $.post( "./api/quizzes", { "answer": inputAnswer })
         .done(function( res ) {
             if (inputAnswer == answer){
+                console.log("1");
                 $("#answerBox button").unbind();
                 totalMagQ1.complete(); 
             }
@@ -64,6 +65,61 @@ function quizMag3(){
             if (inputAnswer == answer){
                 $("#answerBox button").unbind();
                 totalMagQ3.complete(); 
+            }
+            $("#answerBox input").val("");
+        });
+
+    });
+}
+*/
+
+
+   var quiz = [{
+            "answer": "400X"
+            , "setup": function () {
+                ms.diaphragmLightPosition = 40;
+                ms.xcaliper = ms.xslide = -4;
+                ms.eyepiecePosition = 0;
+                ms.ycaliper = ms.yslide = 20;
+                setup(true, true, 1)
+              
+            }}
+            , {
+                "answer": "100X", "setup": function(){setup(false, true, 3)}
+            }
+            , {
+                "answer": "40X"
+                , "setup": function(){setup(true, true, 9)}
+            }]
+
+
+
+
+
+function setup(bool1,bool2,lensPosition){
+
+  ms.rotateLensesCount(ms, bool1, bool2, function(){}, lensPosition); 
+    ms.updateLensesState(ms);
+    ms.slideBlur = 0;
+    ms.update();
+
+}
+
+
+function quizMag(questionNum){
+ 
+   var answer= quiz[questionNum-1].answer
+   quiz[questionNum-1].setup();
+    $("#answerBox button").click(function(){
+        inputAnswer = $("#answerBox input").val();
+        $.post( "./api/quizzes", { "answer": inputAnswer })         // This is to be modified
+        .done(function( res ) {                                     // to be at the end
+                           console.log(inputAnswer , answer)
+            if (inputAnswer == answer){
+
+                $("#answerBox button").unbind();
+                //make totalMaqQ and array instead 
+                eval("totalMagQ"+questionNum+".complete()"); 
             }
             $("#answerBox input").val("");
         });
