f9aad589e0f73190c23450ee5874c2652f44a246
Ken Fehling <me@kenfehling.com>
2017-08-01 18:23:20 -0400

    Oops, no... this

--- js/statemachine.js
+++ js/statemachine.js
@@ -415,43 +415,6 @@ class StateMachine {
         // Low knob
         function addCaliperXDrag(part) {
             registerKnob(part, Directions.VERTICAL, function(rotation) {
-            var val = 1;
-            $(part)
-                .mousedown(function() {
-                    isDown = true;
-                })
-                .mousemove(function(event) {
-                    if (isDown) {
-                        if ((prevX < event.pageX)) {
-                            if (_this.xcaliper < sm_orig["MAX_X_CALIPER"]) {
-                                _this.xcaliper += val;
-                                _this.xslide += val;
-                            }
-                        } else if ((prevX > event.pageX)) {
-                            if (_this.xcaliper > sm_orig["MIN_X_CALIPER"]) {
-                                _this.xcaliper -= val;
-                                _this.xslide -= val;
-                            }
-                        }
-                        prevX = event.pageX;
-
-                        // Blur out if out of magic bounds
-                        if (_this.xcaliper > -10 && this.xcaliper < 10 && this.ycaliper > -10 && this.ycaliper < 10) {
-                            _this.inBounds = true;
-                        } else {
-                            _this.inBounds = false;
-                        }
-                        _this.update();
-                    }
-                })
-                .mouseup(function() {
-                    isDown = false;
-                })
-                .mouseleave(function() {
-                    isDown = false;
-                });
-
-            registerKnob(part, Directions.VERTICAL, function(rotation) {
                 console.log(rotation);
                 _this.xcaliper = rotation / 10;
                 _this.xslide = rotation / 10;
